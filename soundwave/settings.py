60273eabc4b580d6a53f8c0a1155cf1d1caf4dca
Rhain Urmston <rhainu@hotmail.co.uk>
2025-10-29 17:57:06 +0000

    readies code for deploy

--- soundwave/settings.py
+++ soundwave/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^3g0&s-q$5gsomanf$h)6x0(p0&@5#74+!et-(qw*p7@39n=+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
