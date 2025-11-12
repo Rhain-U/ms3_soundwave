79e11016c7b4bd9a6ddc44fb3466f1bbffcbe3b1
Rhain Urmston <rhainu@hotmail.co.uk>
2025-11-12 22:09:09 +0000

    Add Comment section to admin ready for model build.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event_Post, Comment
 
 # Register your models here.
-admin.site.register(Event)
-
+admin.site.register(Event_Post)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,15 @@ from django.contrib.auth.models import User
 STATUS = ((0,"Draft"), (1,"Published"))
 
 # Create your models here.
-class Event(models.Model):
+class Event_Post(models.Model):
     event_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "event")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "event_post")
     title = models.CharField(max_length=200)
     content = models.TextField()
     slug = models.SlugField(max_length = 200, unique = True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices= STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
 
 
 
