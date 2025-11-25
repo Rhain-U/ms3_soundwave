f40179a25709642846540df451cbd33a3001d722
Rhain Urmston <rhainu@hotmail.co.uk>
2025-11-25 18:36:21 +0000

    Linked comments to a specific event.

--- blog/models.py
+++ blog/models.py
@@ -16,14 +16,12 @@ class Event_Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    comment_id = models.AutoField(primary_key= True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    content = models.TextField(max_length=500)
+    comment_id = models.AutoField(primary_key=True)
+    event_id = models.ForeignKey(Event_Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "comment_author")
+    body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(max_length = 200)
-    status = models.IntegerField(choices = STATUS, default = 0)
+    approved = models.BooleanField(default=False)
 
 
-
-
- 
+    
